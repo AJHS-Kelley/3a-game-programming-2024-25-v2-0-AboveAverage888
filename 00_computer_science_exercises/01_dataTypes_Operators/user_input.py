84d372a15a208b03498c5c6430c63ecb311c4086
AboveAverage888 <s11761509@students.duvalschools.org>
2024-09-04 13:20:28 -0400

    wrapper functions

--- 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -20,8 +20,27 @@ print(myNumber + 5)
 
 # Wrapper Functions
 # int() will convert the data to an INTERGER if possible
+newNumber = input("Please type a value and press enter.\n")
+print(int(newNumber)) # can convert STRING to INTERGER
+print(float(newNumber)) # can convert STRING to FLOAT
+print(str(newNumber)) # can convert INTERGER to STRING
+
 # float() will convert the data to a FLOAT if possible
+newNumber = input("Please type a value and press enter.\n")
+#print(int(newNumber)) <-- cannot convert FLOAT to INTERGER
+print(float(newNumber)) # can convert STRING to FLOAT
+print(str(newNumber)) # can convert FLOAT to STRING
+
 # str() will convert the data to a STRING if possible
+newNumber = 5
+print(newNumber + newNumber) # Should print 10
+print(str(newNumber + newNumber)) # Should print 55
+
+
+
+
+
+
 
 
 
