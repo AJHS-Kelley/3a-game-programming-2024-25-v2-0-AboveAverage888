9cc2e00e9505f72ea54c2e762e604dd4a098c5ca
AboveAverage888 <s11761509@students.duvalschools.org>
2024-09-10 13:21:47 -0400

    Awarding New Life

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awardingExtraLives.py
@@ -0,0 +1,24 @@
+# Awarding Extra Lives, Jackques Williams, v0.0
+
+
+lives = 3
+score = input
+# Allow the user to input the score AS AN INTERGER
+# If score is 1000 or less 
+    # Lose a life
+# If score is > 10000 but less than 10001
+    # Give 1 extra life
+# If score is > 1000000
+    # Give 2 extra lives 
+
+# Output the socre and the number of lives to the screen
+
+if score <= 1000:
+    print("You lose, one life")
+
+if score > 10000:
+    print("You will gain a extra life")
+
+
+
+
